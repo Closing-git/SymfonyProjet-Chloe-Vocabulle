54be9dd16708f2a9d605330c33604f038c5bfee3
Chloé Fersing <chloe.fersing@yahoo.fr>
2025-09-12 11:59:28 +0200

    different examples of creating Controller + twig files

---
+++ src/Controller/ExempleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ExempleController extends AbstractController
+{
+    #[Route('/exemple', name: 'app_exemple')]
+    public function index(): Response
+    {
+        return $this->render('exemple/index.html.twig', [
+            'controller_name' => 'ExempleController',
+        ]);
+    }
+
+    #[Route('/exemple/bonjour')]
+    public function bonjour()
+    {
+        return $this->render('exemple/bonjour.html.twig');
+    }
+}
