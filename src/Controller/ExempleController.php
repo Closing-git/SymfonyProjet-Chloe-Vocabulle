4645339e3ab42c29b0be182530e8e8a1111be730
Chloé Fersing <chloe.fersing@yahoo.fr>
2025-09-12 12:14:36 +0200

    insert example

--- src/Controller/ExempleController.php
+++ src/Controller/ExempleController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Langue;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ExempleController extends AbstractController
 {
@@ -20,5 +22,17 @@ final class ExempleController extends AbstractController
     public function bonjour()
     {
         return $this->render('exemple/bonjour.html.twig');
+    }
+
+    #[Route('/exemple/insert')]
+    //Entity Manager Interface permet d'obtenir directement manager sans passer par doctrine
+    public function insertLangue(EntityManagerInterface $manager){
+        $espagnol=new Langue();
+        $espagnol->setNom("espagnol");
+        $espagnol->setMajImportante(false);
+        $espagnol->setCaracteresSpeciaux(["á", "é", "í", "ó", "ú", "ü", "ñ"]);
+        $manager->persist($espagnol);
+        $manager->flush();
+        return $this->render('exemple/insert.html.twig');
     }
 }
