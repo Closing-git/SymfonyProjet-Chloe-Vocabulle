80d2df353c0cdf2cf501346cff814fe40b73d90f
Chloé Fersing <chloe.fersing@yahoo.fr>
2025-09-18 10:48:09 +0200

    Create UtilisateurFixtures

--- migrations/Version20250918084536.php
+++ migrations/Version20250918084536.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250918080918 extends AbstractMigration
+final class Version20250918084536 extends AbstractMigration
 {
     public function getDescription(): string
     {
