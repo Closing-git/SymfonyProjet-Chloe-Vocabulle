ca923ac1a6464e0b7515f3aa4e034f0d62b908c4
Chloé Fersing <chloe.fersing@yahoo.fr>
2025-09-25 12:16:12 +0200

    Adding SearchForm (controller + type + twig)

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\ListeVocabulaire;
+use App\Form\SearchFiltersListesVocabulaireType;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -16,28 +17,11 @@ final class AccueilController extends AbstractController
     #[Route('/accueil', name: 'app_accueil')]
     public function index(EntityManagerInterface $manager, Request $req): Response
     {
-
-
-        // Tri par langues
-        $idLangue = $req->get('id_langue');
-
-        if ($idLangue == "all" || is_null($idLangue)) {
-            $rep = $manager->getRepository(ListeVocabulaire::class);
-            $rep2 = $manager->getRepository(Langue::class);
-            $listesVocabulaire = $rep->findAll();
-            $langues = $rep2->findAll();
-        } else {
-            $rep = $manager->getRepository(ListeVocabulaire::class);
-            $rep2 = $manager->getRepository(Langue::class);
-            $langues = $rep2->findAll();
-            $langueSelected = $rep2->find($idLangue);
-            $listesVocabulaire = $langueSelected->getListesVocabulaire();
-        }
-
-
-
+        $form = $this->createForm(SearchFiltersListesVocabulaireType::class);
+        $form->handleRequest($req);
+        $listesVocabulaire = $manager->getRepository(ListeVocabulaire::class)->findAll();
         
-        $vars = ['listesVocabulaire' => $listesVocabulaire, 'langues' => $langues];
+        $vars = ['listesVocabulaire' => $listesVocabulaire, 'form' =>$form];
         //On envoie le array vars au template en l'ajoutant aux paramètres de render
         return $this->render('accueil/index.html.twig', $vars);
     }
---
+++ src/Form/SearchFiltersListesVocabulaireType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Langue;
+use Doctrine\DBAL\Types\BooleanType;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
+
+class SearchFiltersListesVocabulaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('langue', EntityType::class, [
+                'required' => false,
+                'class' => Langue::class,
+                'placeholder' => 'Toutes les langues',
+                'label' => 'Choix langue',
+                'choice_label' => "nom",
+            ])
+            ->add('ordre', ChoiceType::class, [
+                'required' => false,
+                'placeholder' => 'Choisir le type de tri...',
+                'label' => 'Tri',
+                'choices' => [
+                    'A -> Z' => 'alpha',
+                    'Z -> A' => 'antiAlpha',
+                    'Du plus ancien au plus récent' => 'olderFirst',
+                    'Du plus récent au plus ancien' => 'newerFirst',
+                    'Mieux notés' => 'bestNoteFirst',
+                    'Meilleur score en premier' => 'bestScoreFirst',
+                    'Moins bon score en premier' => 'worseScoreFirst',
+                ]
+            ])
+            ->add('statut', ChoiceType::class, [
+                'required' => false,
+                'label' => 'Public/Privé',
+                'placeholder' => 'Tous',
+                'choices' => [
+                    'Privé' => false,
+                    'Public' => true,
+                ]
+            ])
+            ->add('ownCreator', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Créé par moi ?',
+            ])
+            ->add('fav', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Montrer uniquement les favoris',
+            ])
+            ->add('title', TypeTextType::class, [
+                'required' => false,
+                'label' => 'Rechercher par titre',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
