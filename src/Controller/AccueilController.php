91a8911df5882eaf78cf4216f3e77bd0fd92048d
Chloé Fersing <chloe.fersing@yahoo.fr>
2025-09-25 11:03:30 +0200

    select by langues résolution bug

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -14,32 +14,31 @@ use Symfony\Component\HttpFoundation\Request;
 final class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(EntityManagerInterface $manager, Request $req): Response {
-    
-    $id=$req->get('id_langue');
-
-    if ($id == "all")
+    public function index(EntityManagerInterface $manager, Request $req): Response
     {
-            $rep=$manager->getRepository(ListeVocabulaire::class);
-            $rep2=$manager->getRepository(Langue::class);
-            $listesVocabulaire=$rep->findAll();
-            $langues=$rep2->findAll();
-    }
 
-        else {
-            $rep=$manager->getRepository(ListeVocabulaire::class);
-            $rep2=$manager->getRepository(Langue::class);
-            $langues=$rep2->findAll();
-            $langueSelected=$rep2->find($id);
-            $listesVocabulaire=$langueSelected->getListesVocabulaire();
 
+        // Tri par langues
+        $idLangue = $req->get('id_langue');
+
+        if ($idLangue == "all" || is_null($idLangue)) {
+            $rep = $manager->getRepository(ListeVocabulaire::class);
+            $rep2 = $manager->getRepository(Langue::class);
+            $listesVocabulaire = $rep->findAll();
+            $langues = $rep2->findAll();
+        } else {
+            $rep = $manager->getRepository(ListeVocabulaire::class);
+            $rep2 = $manager->getRepository(Langue::class);
+            $langues = $rep2->findAll();
+            $langueSelected = $rep2->find($idLangue);
+            $listesVocabulaire = $langueSelected->getListesVocabulaire();
         }
-        $vars = ['listesVocabulaire'=>$listesVocabulaire, 'langues'=>$langues];
+
+
+
+        
+        $vars = ['listesVocabulaire' => $listesVocabulaire, 'langues' => $langues];
         //On envoie le array vars au template en l'ajoutant aux paramètres de render
         return $this->render('accueil/index.html.twig', $vars);
     }
-
-
 }
-
-
