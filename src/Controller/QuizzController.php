b06545c537431078820045d8ab451353ed3b9d5c
Chloé Fersing <chloe.fersing@yahoo.fr>
2025-11-04 11:32:51 +0100

    QuizzController in Difficult Mode

--- src/Controller/QuizzController.php
+++ src/Controller/QuizzController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\QuizzType;
+use App\Form\ReponseType;
 use Doctrine\ORM\EntityManager;
 use App\Entity\ListeVocabulaire;
 use Doctrine\ORM\EntityManagerInterface;
@@ -43,24 +44,120 @@ final class QuizzController extends AbstractController
         $liste = $em->getRepository(ListeVocabulaire::class)->find($id_liste);
         $langueCible = $request->query->get('langue_cible');
         $difficulte = $request->query->get('difficulte');
+        //Récupérer les mots et les mélanger
+        $traductions = $liste->getTraduction()->toArray();
+        shuffle($traductions);
+
+
         if ($liste->getLangues()[1]->getNom() == $langueCible) {
             $majStatut = $liste->getLangues()[1]->isMajImportante();
         } else {
             $majStatut = $liste->getLangues()[0]->isMajImportante();
         }
 
+        //Créer la session et l'initialiser
+        $session = $request->getSession();
+        $questions = $session->get('questions', []);
+        $i_question = $session->get('current_question', 0);
+        $score = $session->get('score', 0);
+        if ($questions == []) {
+            $questions = $traductions;
+            $session->set('questions', $questions);
+            $session->set('current_question', 0);
+            $session->set('score', 0);
+        }
+        $currentQuestion = $questions[$i_question] ?? null;
+
+
+        //Quizz en fonction de chaque difficulté :
         if ($difficulte == "difficile") {
-            $p = "Ca va être difficile";
+            $p_difficulte = "Difficile";
+
+            $Reponseform = $this->createForm(ReponseType::class);
+            $Reponseform->handleRequest($request);
+
+            //Quand le quizz est fini (toutes les questions répondues)
+            if ($i_question >= count($questions)) {
+                //Supprimer les données de la session, mais stocker le score final
+                $score_final = $session->get('score');
+                $session->remove('questions');
+                $session->remove('current_question');
+                $session->remove('score');
+
+                return $this->render('quizz/quizz_resultat.html.twig', [
+                    'score_final' => $score_final,
+                    'liste' => $liste,
+                    'p_difficulte' => $p_difficulte,
+                ]);
+            }
+
+            //En fonction de la langue cible, afficher le bon mot à traduire
+            if ($liste->getLangues()[1]->getNom() == $langueCible) {
+                $a_traduire = $currentQuestion->getMotLangue2();
+            } else {
+                $a_traduire = $currentQuestion->getMotLangue1();
+            }
+
+
+            if ($Reponseform->isSubmitted() && $Reponseform->isValid()) {
+                //En fonction de la langue cible, récupérer la bonne réponse
+                if ($liste->getLangues()[1]->getNom() == $langueCible) {
+                    $reponse = $Reponseform->get('reponse')->getData();
+                    $bonneReponse = $currentQuestion->getMotLangue1();
+                } else {
+                    $reponse = $Reponseform->get('reponse')->getData();
+                    $bonneReponse = $currentQuestion->getMotLangue2();
+                }
+
+
+                //Si la maj est importante, tous les caractères doivent être respectés
+                if ($majStatut == true) {
+                    if ($reponse == $bonneReponse) {
+                        $score++;
+                        $session->set('score', $score);
+                    }
+                }
+                //Sinon on met tout en minuscule
+                else {
+                    if (strtolower($reponse) == strtolower($bonneReponse)) {
+                        $score++;
+                        $session->set('score', $score);
+                    }
+                }
+                $i_question++;
+                $session->set('current_question', $i_question);
+
+                return $this->redirectToRoute('app_quizz', [
+                    'id_liste' => $id_liste,
+                    'langue_cible' => $langueCible,
+                    'difficulte' => $difficulte,
+                ]);
+            }
+
+
+            return $this->render('quizz/quizz_questions.html.twig', [
+                'Reponseform' => $Reponseform->createView(),
+                'question' => $currentQuestion,
+                'i_question' => $i_question,
+                'score' => $score,
+                'a_traduire' => $a_traduire,
+                'p_difficulte' => $p_difficulte,
+                'liste' => $liste,
+                'langue_cible' => $langueCible,
+                'majStatut' => $majStatut,
+            ]);
         } elseif ($difficulte == "moyen") {
-            $p = "Ca va être moyen";
+            $Reponseform = $this->createForm(ReponseType::class);
+            $Reponseform->handleRequest($request);
+            $p_difficulte = "Moyen";
         } else {
-            $p = "Ca va être facile";
+            $Reponseform = $this->createForm(ReponseType::class);
+            $Reponseform->handleRequest($request);
+            $p_difficulte = "Facile";
         }
 
-        $i_question = 1;
 
-
-        $vars = ["p" => $p, "liste" => $liste, "langue_cible" => $langueCible, "majStatut" => $majStatut, "i_question" => $i_question];
+        $vars = ["Reponseform" => $Reponseform, "score" => $score, "p_difficulte" => $p_difficulte, "liste" => $liste, "langue_cible" => $langueCible, "majStatut" => $majStatut, "i_question" => $i_question];
         return $this->render('quizz/quizz_questions.html.twig', $vars);
     }
 }
