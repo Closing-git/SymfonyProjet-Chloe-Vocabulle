184d4b9a412c5a74bf36e23f261e84db6e65c91e
Chloé Fersing <chloe.fersing@yahoo.fr>
2025-11-19 10:31:35 +0100

    Change in globa CSS + log page

--- migrations/Version20251119090449.php
+++ migrations/Version20251119090449.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251007094847 extends AbstractMigration
+final class Version20251119090449 extends AbstractMigration
 {
     public function getDescription(): string
     {
