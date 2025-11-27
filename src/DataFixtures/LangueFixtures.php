7290fa0e9433aa897754a8a8637baea940cc5a2f
Chloé Fersing <chloe.fersing@yahoo.fr>
2025-11-27 10:08:31 +0100

    Fixtures ENG FR DE

--- src/DataFixtures/LangueFixtures.php
+++ src/DataFixtures/LangueFixtures.php
@@ -13,19 +13,55 @@ class LangueFixtures extends Fixture
     {
         $faker = Faker\Factory::create('fr_FR');
 
-        for ($i=0; $i<10; $i++){
+        for ($i = 0; $i < 10; $i++) {
             $langue = new Langue();
             $langue->setNom($faker->countryCode());
             $langue->setMajImportante($faker->boolean(50));
 
             $arrayCaracSpeciaux = [];
-            for ($i2=0; $i2<$faker->numberBetween(0, 7); $i2++){
+            for ($i2 = 0; $i2 < $faker->numberBetween(0, 7); $i2++) {
                 $arrayCaracSpeciaux[] = $faker->randomLetter();
             }
             $langue->setCaracteresSpeciaux($arrayCaracSpeciaux);
-            $this->addReference('langue'. $i, $langue);
+            $this->addReference('langue' . $i, $langue);
             $manager->persist($langue);
         }
+
+        // FR - Français
+        $FR = new Langue();
+        $FR->setNom("FR");
+        $FR->setMajImportante(false);
+
+        $arrayCaracSpeciaux = ['â', 'à', 'ç', 'é', 'è', 'ê', 'ë', 'î', 'ï', 'ô', 'œ', 'ù', 'û'];
+        $FR->setCaracteresSpeciaux($arrayCaracSpeciaux);
+        $this->addReference('FR', $FR);
+        $manager->persist($FR);
+
+        //ENG - Anglais
+
+        $ENG = new Langue();
+        $ENG->setNom("ENG");
+        $ENG->setMajImportante(false);
+
+        $arrayCaracSpeciaux = [];
+        $ENG->setCaracteresSpeciaux($arrayCaracSpeciaux);
+        $this->addReference('ENG', $ENG);
+        $manager->persist($ENG);
+
+        //DE - Allemand
+
+        $DE = new Langue();
+        $DE->setNom("DE");
+        $DE->setMajImportante(true);
+
+        $arrayCaracSpeciaux = ['ä', 'Ä', 'ö', 'Ö', 'ü', 'Ü', 'ß'];
+        $DE->setCaracteresSpeciaux($arrayCaracSpeciaux);
+        $this->addReference('DE', $DE);
+        $manager->persist($DE);
+
+        
+
+
 
         $manager->flush();
     }
